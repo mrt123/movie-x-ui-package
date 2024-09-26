57fa36526cf28f2950393e341b9d42c76c08d10f
Tomasz Sz <tomasz.szawara@gmail.com>
2024-09-26 12:21:09 +0200

    fix(mx-5): removed npm run build

--- release.config.js
+++ release.config.js
@@ -14,12 +14,14 @@ module.exports = {
         assets: [
           {
             path: "dist/out/**/*.*",
-            label: "static dist", // to avoid "Request quota exhausted for request GET /search/issues"
+            label: "static dist",
             name: "movie-x-ui",
           },
         ],
         draftRelease: true,
+        // successComment and failTitle = false: to avoid "Request quota exhausted for request GET /search/issues"
         successComment: false,
+        failTitle: false,
       },
     ],
   ],
